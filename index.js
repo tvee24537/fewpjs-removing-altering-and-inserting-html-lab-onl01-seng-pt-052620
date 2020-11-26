4a04120a0a56ce655b05d1a6da42f9f8a61a8576
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-26 19:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = 'YOUR_NAME is the champion';
+document.body.appendChild(newHeader); 
